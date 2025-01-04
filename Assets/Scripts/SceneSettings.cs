c0e30a43b3db4be90920d304fcd6a104655252fc
h <r121338@g.kobe-kosen.ac.jp>
2025-01-05 01:05:15 +0900

    movecan

--- Assets/Scripts/SceneSettings.cs
+++ Assets/Scripts/SceneSettings.cs
@@ -8,6 +8,9 @@ public class SceneSettings : MonoBehaviour
     // エンディングに進むための進行回数
     [SerializeField] public int requiredScenesToAdvance = 3;
 
+    // 進行したシーンの数を保持する静的変数
+    public static int scenesProgressed = 0;
+
     // このスクリプトを通して、他のスクリプトで設定を取得できるようにする
     public int GetMaxSceneID()
     {
@@ -17,5 +20,11 @@ public class SceneSettings : MonoBehaviour
     public int GetRequiredScenesToAdvance()
     {
         return requiredScenesToAdvance;
+    }
+
+    // シーンIDが0の場合、ランダムで1から最大のところに飛ぶ
+    public int GetRandomSceneID()
+    {
+        return Random.Range(1, maxSceneID + 1);
     }
 }
